ea01973f63dae2834d513a98282d8ffce2d84cf7
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2022-12-18 11:12:42 -0800

    8-print_base16.c created

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+  * main - prints all hexadecimal numbers
+  * Return: 0
+  */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+
+	for (n = 'A'; n <= 'F'; n++)
+	{
+		putchar(n);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
