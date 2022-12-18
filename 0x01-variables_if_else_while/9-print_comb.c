5f4396060d917b53ac20efc950f744f3283b54ee
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2022-12-18 12:43:44 -0800

    9-print_comb.c modified

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,22 +2,24 @@
 
 /**
   * main - prints every possible combination of single digits
-  * Return - 0
+  * Return: 0
   */
 int main(void)
 {
-	int n1, n2;
+	int n;
 
-	for (n1 = 0; n1 < 10; n1++)
+	for (n = 0; n < 10; n++)
 	{
-		for (n2 = 0; n2 < 10; n2++)
-		{
-			putchar((n1 % 10) + '0');
-			putchar((n2 % 10) + '0');
-			putchar(',');
-			putchar(' ');
-		}
+		putchar((n % 10) + '0');
+
+		if (n == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
 	}
+
+	putchar('\n');
 
 	return (0);
 }
