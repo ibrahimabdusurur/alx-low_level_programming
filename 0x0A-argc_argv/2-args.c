d7103c2ab54f1bf53eec7a9dfa032edc294c88e0
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-06 11:43:04 -0800

    2-args.c created

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - that prints all arguments it receives.
+  * @argc: The number of command line arguments.
+  * @argv: An array containing the program command line arguments.
+  *
+  * Return: 0.
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
