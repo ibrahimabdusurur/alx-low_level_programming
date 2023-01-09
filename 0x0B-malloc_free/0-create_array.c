3149c734f970e3fac966d0f231be591c5313af90
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-09 08:26:31 -0800

    0-create_array.c modified

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,9 +16,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return ('\0');
+		return (NULL);
 
 	str = malloc(sizeof(char) * size);
+
+	if (str == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		str[i] = c;
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,5 +1,6 @@
 ## 0x0B. C - malloc, free
 
 * 0-create_array.c  
-
-function that creates an array of chars, and initializes it with a specific char.
+>
+> function that creates an array of chars, and initializes it with a specific char.
+>
