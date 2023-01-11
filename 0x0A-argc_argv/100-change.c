2338a828eef264a52e1b989938cf45f43a1b44a0
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-11 06:45:16 -0800

    100-change.c modified

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -26,7 +26,7 @@ int main(int argc, char *argv[])
 
 	else
 	{
-		coins = (cents / 25) + ((cents % 25) / 10) + (((cents % 25) % 10) / 5) + 
+		coins = (cents / 25) + ((cents % 25) / 10) + (((cents % 25) % 10) / 5) +
 			((((cents % 25) % 10) % 5) / 2) + ((((cents % 25) % 10) % 5) % 2);
 
 		printf("%d\n", coins);
