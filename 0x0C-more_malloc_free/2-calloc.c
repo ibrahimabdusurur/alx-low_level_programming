166973d4a5cafc4ce89b0afa94c160df47d10d5b
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-13 08:25:20 -0800

    2-calloc.c modified

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,33 +1,25 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * _calloc - Allocates memory for an array of a certain number
- *           of elements each of an inputted byte size.
- * @nmemb: The number of elements.
- * @size: The byte size of each array element.
- *
- * Return: If nmemb = 0, size = 0, or the function fails - NULL.
- *         Otherwise - a pointer to the allocated memory.
- */
+  * _calloc - allocates memory for an array, using malloc.
+  * @nmemb: number of elements of the array.
+  * @size: memory bytes of each element of the array.
+  *
+  * Return: NULL if nmemb is 0, size is 0 or malloc fails.
+  *	    a pointer to the allocated memory otherwise.
+  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *mem;
-	char *filler;
-	unsigned int index;
+	void *ptr;
 
-	if (nmemb == 0 || size == 0)
+	if ((nmemb == 0) || (size == 0))
 		return (NULL);
 
-	mem = malloc(size * nmemb);
+	ptr = malloc(nmemb * size);
 
-	if (mem == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
-	filler = mem;
-
-	for (index = 0; index < (size * nmemb); index++)
-		filler[index] = '\0';
-
-	return (mem);
+	return (ptr);
 }
