8cd61441dcc62d84722593bb3b747ed5f57c375c
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-13 08:52:38 -0800

    2-calloc.c modified

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,6 +12,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *ptr;
+	char *bytes;
+	unsigned int i;
 
 	if ((nmemb == 0) || (size == 0))
 		return (NULL);
@@ -20,6 +22,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	if (ptr == NULL)
 		return (NULL);
+
+	bytes = ptr;
+
+	for (i = 0; i < (nmemb *size); i++)
+		bytes[i] = '\0';
 
 	return (ptr);
 }
