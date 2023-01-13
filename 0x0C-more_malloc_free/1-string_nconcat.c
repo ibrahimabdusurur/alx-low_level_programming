60fbd33ce7cd5a959eaf615acd585da753448509
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-13 05:29:27 -0800

    1-string_nconcat.c modified

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -56,11 +56,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; i < len_s1; i++)
 			ptr[i] = s1[i];
 
-		for (i = 0; i <= n; i++)
+		for (i = 0; i < n; i++)
 		{
 			ptr[len_s1] = s2[i];
 			len_s1++;
 		}
+
+		ptr[len_s1] = '\0';
 	}
 
 	return (ptr);
