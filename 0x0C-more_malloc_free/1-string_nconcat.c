fd20222d4fbeb7339726d37054dd41c3953ed8bb
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-13 05:07:29 -0800

    1-string_nconcat.c modified

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,18 +23,15 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	while (s1[len_s1++])
+	while (*(s1 + len_s1))
 		len_s1++;
 
-	while (s2[len_s2++])
+	while (*(s2 + len_s2))
 		len_s2++;
 
-	len_s1--;
-	len_s2--;
-
-	if (((len_s1 == 0) || (len_s2 == 0)) && (n >= len_s2))
+	if (n >= len_s2)
 	{
-		ptr = malloc(sizeof(char) * (len_s1 + len_s2));
+		ptr = malloc(sizeof(char) * (len_s1 + len_s2 + 1));
 
 		if (ptr == NULL)
 			return (NULL);
@@ -42,31 +39,14 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; i < len_s1; i++)
 			ptr[i] = s1[i];
 
-		for (i = 0; i < len_s2; i++)
+		for (i = 0; i <= len_s2; i++)
 		{
-			ptr[i] = s2[i];
-			i++;
-		}
-	}
-
-	if ((len_s1 != 0) && (len_s2 != 0) && (n >= len_s2))
-	{
-		ptr = malloc(sizeof(char) * (len_s1 + len_s2 - 1));
-
-		if (ptr == NULL)
-			return (NULL);
-
-		for (i = 0; i < len_s1; i++)
-			ptr[i] = s1[i];
-
-		for (i = 0; i < (len_s2 - 1); i++)
-		{
-			ptr[len_s1 - 1] = s2[i];
+			ptr[len_s1] = s2[i];
 			len_s1++;
 		}
 	}
 
-	if (((len_s1 == 0) || (len_s2 == 0)) && (n < len_s2))
+	else if (n < len_s2)
 	{
 		ptr = malloc(sizeof(char) * (len_s1 + n + 1));
 
@@ -76,26 +56,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		for (i = 0; i < len_s1; i++)
 			ptr[i] = s1[i];
 
-		for (i = 0; i < (n + 1); i++)
+		for (i = 0; i <= n; i++)
 		{
-			ptr[i] = s2[i];
-			i++;
-		}
-	}
-
-	if ((len_s1 != 0) && (len_s2 != 0) && (n < len_s2))
-	{
-		ptr = malloc(sizeof(char) * (len_s1 + n));
-
-		if (ptr == NULL)
-			return (NULL);
-
-		for (i = 0; i < len_s1; i++)
-			ptr[i] = s1[i];
-
-		for (i = 0; i < n; i++)
-		{
-			ptr[len_s1 - 1] = s2[i];
+			ptr[len_s1] = s2[i];
 			len_s1++;
 		}
 	}
