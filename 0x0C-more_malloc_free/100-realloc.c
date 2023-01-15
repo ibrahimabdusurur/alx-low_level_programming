d0d84160226dd275ce78c9b9b898fadc6452325b
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-15 09:12:17 -0800

    100-realloc.c modified

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,18 +13,31 @@
   */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
+	void *new_ptr;
+	char *filler, *ptr_copy, *new_ptr_copy;
+	unsigned int i;
+
 	if (new_size == 0 && ptr != NULL)
 		return (NULL);
 
 	if (old_size == new_size)
 		return (ptr);
 
-	free(ptr);
+	new_ptr = malloc(new_size);
 
-	ptr = malloc(new_size);
-
-	if (ptr == NULL)
+	if (new_ptr == NULL)
 		return (NULL);
 
-	return (ptr);
+	filler = new_ptr;
+	new_ptr_copy = new_ptr;
+	ptr_copy = ptr;
+
+	for (i = 0; *ptr_copy++ && *new_ptr_copy; i++)
+		filler[i] = '\0';
+
+	filler[i] = '\0';
+
+	free(ptr);
+
+	return (new_ptr);
 }
