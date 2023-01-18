c5cbc903d81e1fdee8064e347fed7190854892c8
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-18 05:55:31 -0800

    0-print_name.c created

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+  * print_name - prints a name.
+  * @name: the name to be printed.
+  * @f: the function that points to a function that prints the name.
+  */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,8 @@
+## 0x0F-function_pointers  
+  
+#### 0-print_name.c  
+  
+> a function that prints a name.  
+>
+> * Prototype:  
+> void print_name(char *name, void (*f)(char *));
