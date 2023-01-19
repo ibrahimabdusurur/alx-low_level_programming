53f75862f31582eea7683fe9ec25a96907e60d97
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-19 03:22:58 -0800

    1-print_numbers.c modified

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,10 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list num;
 	unsigned int i;
 
+	va_start(num, n);
+
 	if (separator == NULL)
 		separator = "";
-
-	va_start(num, n);
 
 	for (i = 0; i < n - 1; i++)
 		printf("%d%s", va_arg(num, int), separator);
