b9d232bbb276cce8e913c6cedded67a944538383
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-26 12:09:38 -0800

    0-print_list.c modified

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,7 +9,7 @@
   */
 size_t print_list(const list_t *h)
 {
-	int l = 0;
+	size_t nodes = 0;
 
 	while (h != NULL)
 	{
@@ -20,8 +20,8 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 
 		h = h->next;
-		l++;
+		nodes++;
 	}
 
-	return (l);
+	return (nodes);
 }
