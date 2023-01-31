aee5838081c33cce1a5e8f9286be429fce6a36d7
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-01-31 02:42:53 -0800

    5-free_listint2.c modified-indentation

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,7 +4,7 @@
   * free_listint2 - frees a listint_t list.
   * @head: points to address of the first node.
   *
-  * desc: The function sets the head to NULL.
+  * description: The function sets the head to NULL.
   */
 void free_listint2(listint_t **head)
 {
