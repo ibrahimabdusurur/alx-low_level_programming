0bff446fccbe1ba0c5c0ef2b4f33e30f0cb831c4
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-02-01 09:52:52 -0800

    100-reverse_listint.c modified - indentation

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,17 +1,16 @@
 #include "lists.h"
 
 /**
- * reverse_listint - Reverses a listint_t list.
- * @head: A pointer to the address of
- *        the head of the list_t list.
- *
- * Return: A pointer to the first node of the reversed list.
- */
+  * reverse_listint - reverses a listint_t linked list.
+  * @head: a pointer that points to the first node.
+  *
+  * Return: a pointer to the first node of the reversed list.
+  */
 listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *ahead, *behind;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL)
 		return (NULL);
 
 	behind = NULL;
@@ -25,6 +24,5 @@ listint_t *reverse_listint(listint_t **head)
 	}
 
 	(*head)->next = behind;
-
 	return (*head);
 }
