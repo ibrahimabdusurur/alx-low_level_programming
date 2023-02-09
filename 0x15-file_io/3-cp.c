eb56c4b2676349c4e8f29c845ee2cbbf1f93a3e4
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-02-09 03:03:04 -0800

    3-cp.c modified - indentation and permisions for open()

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,22 +11,32 @@ void _cp(char *file_from, char *file_to)
 	int fd1, fd2, r_sz, w_sz, cl_1, cl_2;
 	char *c;
 
-	c = malloc(1024);
-	fd1 = open(file_from, O_RDWR);
+	c = malloc(sizeof(char) * 1024);
+
+	if (c == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			 "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+
+	fd1 = open(file_from, O_RDONLY);
 	r_sz = read(fd1, c, 1024);
 
 	if (fd1 == -1 || r_sz == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		dprintf(STDERR_FILENO,
+			 "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
-	fd2 = open(file_to, O_CREAT | O_RDWR | O_TRUNC, 0664);
+	fd2 = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	w_sz = write(fd2, c, r_sz);
 
 	if (fd2 == -1 || w_sz == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		dprintf(STDERR_FILENO,
+			 "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 
@@ -35,7 +45,8 @@ void _cp(char *file_from, char *file_to)
 
 	if (cl_1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd1);
+		dprintf(STDERR_FILENO,
+			 "Error: Can't close fd %d", fd1);
 		exit(100);
 	}
 
@@ -43,13 +54,14 @@ void _cp(char *file_from, char *file_to)
 
 	if (cl_2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d", fd2);
+		dprintf(STDERR_FILENO,
+			 "Error: Can't close fd %d", fd2);
 		exit(100);
 	}
 }
 
 /**
-  * main - 
+  * main - copies the content of a file to another file.
   * @argc: number of arguments passed to the function.
   * @argv: arguments passed to the function.
   *
@@ -59,11 +71,12 @@ int main(int argc, char **argv)
 {
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp %s %s\n", argv[1], argv[2]);
+		dprintf(STDERR_FILENO,
+			 "Usage: cp %s %s\n", argv[1], argv[2]);
 		exit(97);
 	}
 
 	_cp(argv[1], argv[2]);
 
 	return (0);
-}	
+}
