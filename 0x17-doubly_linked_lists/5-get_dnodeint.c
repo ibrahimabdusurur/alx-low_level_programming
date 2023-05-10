fa57995a6e43ef91a5c7bffa3ec8b92d6e3a7898
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-05-10 03:49:34 -0700

    5-get_dnodeint.c indentation

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -5,8 +5,8 @@
   * @head: pointer to the first node.
   * @index: the index of the node, starting from 0.
   *
-  * Return: NULL if the node does not exist,
-  *	    otherwise the nth node of a dlistint_t linked list.
+  * Return: NULL - if the node does not exist,
+  *	    otherwise - the nth node of a dlistint_t linked list.
   */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
