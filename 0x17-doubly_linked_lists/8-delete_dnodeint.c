900e71e84b27f4a8df62881e7d275d088eda185c
ibrahimabdusurur <117864920+ibrahimabdusurur@users.noreply.github.com>
2023-05-11 05:39:27 -0700

    8-delete_dnodeint.c modified

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,11 +13,11 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *tmp = *head;
 
 	if (*head == NULL)
-		return (1);
+		return (-1);
 
 	for (; index != 0; index--)
 	{
-		if (tmp->next == NULL)
+		if (tmp == NULL)
 			return (-1);
 
 		tmp = tmp->next;
